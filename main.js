b32009ea5b1c3912eeb15d1a572df9fc705c2d8c
Haps <thiamhapstou@gmail.com>
2024-07-12 13:14:39 +0000

    validation des données avant la soumission

--- main.js
+++ main.js
@@ -5,29 +5,89 @@ const description = document.getElementById('description');
 const submit_button = document.getElementById('submit-button');
 const cards_section = document.getElementById('cards-section');
 let idees = [];
+// Expressions régulières pour vérifier l'absence de chiffres et de balises HTML
+const regexNoDigits = /^[^\d]*$/; 
+const regexNoHTML = /<\/?[^>]+(>|$)/g; 
+// Liste des catégories valides
+const validCategories = ["Politique", "Sport", "Sante", "Education", "Autre"];
 
 
 // Ajout d'un écouteur d'événement pour la soumission du formulaire
 form.addEventListener('submit', e =>{
     e.preventDefault();
 
-    const libelleValue = libelle.value;
-    const categorieValue = categorie.value;
-    const descriptionValue = description.value;
+    let isValid = true;
 
-    // Ajouter la nouvelle idée à la liste
-    const nouvelleIdee = {
-        libelle: libelleValue,
-        categorie: categorieValue,
-        description: descriptionValue
-    };
+    // Validation du libelle
+    const valeurLibelle = libelle.value.trim();
+    if (valeurLibelle === '') {
+        setError(libelle, 'Le libelle est obligatoire');
+        isValid = false;
+    } else if (valeurLibelle.length < 5 || valeurLibelle.length > 50) {
+        setError(libelle, 'Le libelle doit contenir entre 5 et 50 caractères');
+        isValid = false;
+    } else if (!regexNoDigits.test(valeurLibelle)) {
+        setError(libelle, 'Le libelle ne doit pas contenir de chiffres');
+        isValid = false;
+    } else if (regexNoHTML.test(valeurLibelle)) {
+        setError(libelle, 'Le libelle ne doit pas contenir de balises HTML');
+        isValid = false;
+    } else {
+        setSuccess(libelle);
+    }
 
-    idees.push(nouvelleIdee); // Ajouter la nouvelle idée au tableau
 
-    // Afficher à nouveau toutes les idées (y compris la nouvelle)
-    affichageIdee(idees);
+    // Validation de la categorie
+    const valeurCategorie = categorie.value.trim();
+    if (valeurCategorie === '') {
+        setError(categorie, 'Vous devez choisir une catégorie');
+        isValid = false;
+    } else if (!validCategories.includes(valeurCategorie)) {
+        setError(categorie, "Cette catégorie n'existe pas");
+        isValid = false;
+    } else {
+        setSuccess(categorie);
+    }
 
-    resetForm();
+     // Validation de la description
+     const valeurDescription = description.value.trim();
+     if (valeurDescription === '') {
+         setError(description, 'La description est obligatoire');
+         isValid = false;
+     } else if (valeurDescription.length < 10 || valeurDescription.length > 250) {
+         setError(description, 'La description doit contenir entre 10 et 250 caractères');
+         isValid = false;
+     } else if (!regexNoDigits.test(valeurDescription)) {
+         setError(description, 'La description ne doit pas contenir de chiffres');
+         isValid = false;
+     } else if (regexNoHTML.test(valeurDescription)) {
+        setError(description, 'La description ne doit pas contenir de balises HTML');
+        isValid = false;
+    } else {
+         setSuccess(description);
+     }
+
+    
+    // Si toutes les validations sont réussies, ajouter la nouvelle idée
+    if (isValid) {
+        const libelleValue = libelle.value;
+        const categorieValue = categorie.value;
+        const descriptionValue = description.value;
+
+        // Ajouter la nouvelle idée à la liste
+        const nouvelleIdee = {
+            libelle: libelleValue,
+            categorie: categorieValue,
+            description: descriptionValue
+        };
+
+        idees.push(nouvelleIdee); // Ajouter la nouvelle idée au tableau
+
+        // Afficher à nouveau toutes les idées (y compris la nouvelle)
+        affichageIdee(idees);
+
+        resetForm();
+    }
 });
 
 //fonction pour réinitialiser les idées après soumission
@@ -56,3 +116,22 @@ const affichageIdee = (idees) => {
     });
 }
 
+//fonction pour afficher un message d'erreur 
+const setError = (element, message)  => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+//fonction pour afficher un message de succès
+const setSuccess = (element, message)  => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
